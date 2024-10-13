dd724551a744e827b1bf495c6b5e984f0eb2d3cf
Matias <matcubillosn@gmail.com>
2024-10-13 13:57:09 -0300

    Primer commit backend capstone

---
+++ src/main/java/com/mrna/capstone_app/CapstoneAppApplication.java
@@ -0,0 +1,13 @@
+package com.mrna.capstone_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrna/capstone_app/CapstoneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrna.capstone_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
