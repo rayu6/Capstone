02a032a5ce7cc1a41ffd001687d04a8795d772a1
AngelTapia2024 <an.tapiap@duocuc.cl>
2024-11-24 16:57:11 -0300

    Funcion descuento de stock al agregar pedido

--- proyecto/apt2/core/models.py
+++ proyecto/apt2/core/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-
 class Role(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)  
     nombre_role = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nombre_role
 
 class Usuario(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True) 
     nombre = models.CharField(max_length=45)
     correo = models.EmailField(max_length=255, unique=True)
     hash_pass = models.CharField(max_length=45)
@@ -21,7 +20,7 @@ class Usuario(models.Model):
         return self.nombre
 
 class TipoDeOrden(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)  
     nombre_tipo_de_orden = models.CharField(max_length=45)
 
     def __str__(self):
@@ -30,8 +29,8 @@ class TipoDeOrden(models.Model):
         verbose_name = "Tipo de orden"
         verbose_name_plural = "Tipo de orden"
 class Estado(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
-    nombre_estado = models.CharField(max_length=45)  # Añadí este campo ya que parece que falta en la imagen
+    id = models.AutoField(primary_key=True)  
+    nombre_estado = models.CharField(max_length=45)  
 
     def __str__(self):
         return self.nombre_estado
@@ -39,11 +38,34 @@ class Estado(models.Model):
 
 
 class Pedido(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     tipo_de_orden = models.ForeignKey(TipoDeOrden, on_delete=models.CASCADE)
     estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
     receta_pedido = models.ForeignKey('RecetaPedido', on_delete=models.CASCADE)
+
+    def descontar_ingredientes_del_stock(self):
+        receta_pedido = self.receta_pedido
+        
+        # recorrer ingredientes
+        for receta_ingrediente in receta_pedido.recetas.receta_ingrediente.all():
+            ingrediente = receta_ingrediente.ingrediente
+            cantidad_usada = receta_ingrediente.cantidad 
+            
+            # descontar cantidad de stock, validar que no sobrepase el limite
+            if ingrediente.cantidad >= cantidad_usada:
+                ingrediente.cantidad -= cantidad_usada
+                ingrediente.save()
+            else:
+                raise ValueError(f"No hay suficiente stock de {ingrediente.nombre_ingrediente.nombre}.")
+    
+    # sobrescribir el metodo save para descontar ingredientes despues de crear el pedido
+    def save(self, *args, **kwargs):
+        # Guardar el pedido
+        super().save(*args, **kwargs)
+        
+        # aplicar fucnion descontar 
+        self.descontar_ingredientes_del_stock()
 
 
     
@@ -58,7 +80,7 @@ class Ingrediente(models.Model):
         return self.nombre_ingrediente.nombre
     
 class RecetaIngrediente(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)  
     cantidad = models.DecimalField(max_digits=10, decimal_places=2)
     unidad=models.CharField(max_length=100,null=False)
     ingrediente = models.ForeignKey('Ingrediente', on_delete=models.CASCADE,)
@@ -67,7 +89,7 @@ class RecetaIngrediente(models.Model):
          return f"{self.ingrediente.nombre_ingrediente.nombre} - {self.cantidad} -{self.unidad}"
     
 class RecetaPedido(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)  
     recetas=models.ForeignKey('Recetas',on_delete=models.CASCADE)
 
     def __str__(self):
@@ -75,28 +97,28 @@ class RecetaPedido(models.Model):
     
 
 class NombreIngrediente(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)  
     nombre = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nombre
 
 class Recetas(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True) 
     descripcion = models.CharField(max_length=255)
     link = models.ImageField(upload_to='imagenes_recetas/', null=True, blank=True)
     nombre_receta = models.ForeignKey('NombreReceta', on_delete=models.CASCADE)
     receta_ingrediente = models.ManyToManyField(RecetaIngrediente)
     
     def __str__(self):
-        return str(self.nombre_receta.nombre)  # Asegúrate de acceder al campo de texto en NombreReceta
+        return str(self.nombre_receta.nombre)  
     
     class Meta:
         verbose_name = "Recetas"
         verbose_name_plural = "Recetas"
 
 class NombreReceta(models.Model):
-    id = models.AutoField(primary_key=True)  # Clave primaria
+    id = models.AutoField(primary_key=True)  
     nombre = models.CharField(max_length=255)
 
     def __str__(self):
